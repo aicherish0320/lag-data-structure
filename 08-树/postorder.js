fd8c45551b849f6439c34c5db93d3caa7b06294f
aicherish <aicherish0320@163.com>
2022-03-25 07:41:14 +0800

    feat: 二叉树非递归版先中后序遍历

--- 08-树/postorder.js
+++ 08-树/postorder.js
@@ -1,10 +1,31 @@
 const { bt } = require('./bt')
 
+// const postorder = (root) => {
+//   if (!root) return
+//   postorder(root.left)
+//   postorder(root.right)
+//   console.log(root.val)
+// }
+
 const postorder = (root) => {
   if (!root) return
-  postorder(root.left)
-  postorder(root.right)
-  console.log(root.val)
+
+  const outputStack = []
+  const stack = [root]
+
+  while (stack.length) {
+    const n = stack.pop()
+    outputStack.push(n)
+    n.left && stack.push(n.left)
+    n.right && stack.push(n.right)
+  }
+
+  console.log(outputStack)
+
+  while (outputStack.length) {
+    const n = outputStack.pop()
+    console.log(n.val)
+  }
 }
 
 postorder(bt)
--- 08-树/preorder.js
+++ 08-树/preorder.js
@@ -1,12 +1,22 @@
 const { bt } = require('./bt')
 
+// const preorder = (root) => {
+//   if (!root) {
+//     return
+//   }
+//   console.log(root.val)
+//   preorder(root.left)
+//   preorder(root.right)
+// }
+
 const preorder = (root) => {
-  if (!root) {
-    return
+  const stack = [root]
+  while (stack.length) {
+    const n = stack.pop()
+    console.log(n.val)
+    n.right && stack.push(n.right)
+    n.left && stack.push(n.left)
   }
-  console.log(root.val)
-  preorder(root.left)
-  preorder(root.right)
 }
 
 preorder(bt)

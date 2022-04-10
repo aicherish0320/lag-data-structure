1dc81a03c91993ec4346bd99f87899bb289f19c2
aicherish0320 <aicherish0320@163.com>
2022-04-11 06:59:41 +0800

    合并两个有序链表2

--- 09-排序和搜索/08-合并两个有序链表.js
+++ 09-排序和搜索/08-合并两个有序链表.js
@@ -22,5 +22,26 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function (list1, list2) {
-  while (list1.next || list2.next) {}
+  const ret = new ListNode(0)
+  const p = ret
+  const p1 = list1
+  const p2 = list2
+
+  while (p1 && p2) {
+    if (p1.val > p2.val) {
+      p.next = p1
+      p1 = p1.next
+    } else {
+      p.next = p2
+      p2 = p2.next
+    }
+    p = p.next
+  }
+  if (p1) {
+    p.next = p1
+  }
+  if (p2) {
+    p.next = p2
+  }
+  return ret.next
 }

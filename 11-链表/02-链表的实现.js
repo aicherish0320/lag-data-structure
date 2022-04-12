cf06068f148c164b11e4290ae4a5d6c6fdb0f101
aicherish0320 <aicherish0320@163.com>
2022-04-12 13:17:32 +0800

    feat: 链表实现下

--- 11-链表/02-链表的实现.js
+++ 11-链表/02-链表的实现.js
@@ -23,7 +23,7 @@ class LinkedList {
     this.count = 0
     this.head = null
   }
-  // 添加节点
+  // 尾部添加节点
   addAtTail(val) {
     // 根据传入的数据 创建新的节点
     const node = new LinkedNode(val)
@@ -40,6 +40,67 @@ class LinkedList {
     }
     this.count++
   }
+  // 首部添加节点
+  addAtHead(value) {
+    const node = new LinkedNode(value)
+    if (this.count === 0) {
+      this.head = node
+    } else {
+      // 将 node 添加到 head 前面
+      node.next = this.head
+      this.head = node
+    }
+    this.count++
+  }
+  // 获取节点（根据索引）
+  get(index) {
+    if (this.count === 0 || index < 0 || index >= this.count) {
+      return
+    }
+    // 迭代链表，找到对应节点
+    let current = this.head
+    for (let i = 0; i < index; i++) {
+      current = current.next
+    }
+    return current
+  }
+  // 添加节点（根据索引）
+  addAtIndex(value, index) {
+    if (this.count === 0 || index >= this.count) {
+      return
+    }
+    // 如果 index <= 0，都添加到头部即可
+    if (index <= 0) {
+      return this.addAtHead(value)
+    }
+    // 后面为正常区间处理
+    const prev = this.get(index - 1)
+    const next = prev.next
+
+    const node = new LinkedNode(value)
+    prev.next = node
+    node.next = next
+
+    this.count++
+  }
+  // 删除（根据索引）
+  removeAtIndex(index) {
+    if (this.count === 0 || index < 0 || index >= this.count) {
+      return
+    }
+    if (index === 0) {
+      this.head = this.head.next
+    } else {
+      const prev = this.get(index - 1)
+      prev.next = prev.next.next
+    }
+    this.count--
+  }
 }
 
 const l = new LinkedList()
+
+l.addAtTail('a')
+l.addAtTail('b')
+l.addAtTail('c')
+l.addAtIndex('d', 1)

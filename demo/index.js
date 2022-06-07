9ada3d93a016eacfc30f3ea7f2797ec72f2a134d
aicherish0320 <aicherish0320@163.com>
2022-06-07 17:50:08 +0800

    有效的括号

--- demo/index.js
+++ demo/index.js
@@ -1,11 +1,33 @@
 /**
- * 把一个数组旋转 K 步
+ * 判断一个字符串是否括号匹配
  */
 
-const arr = [1, 2, 3, 4, 5, 6]
-const fn = (arr, k) => {
-  const a1 = arr.slice(0, k)
-  const a2 = arr.slice(k)
-  return a2.concat(a1)
+const str = '[{[}]]'
+
+const fn = (str) => {
+  if (str.length % 2 !== 0) return false
+  // 栈
+  const stack = []
+
+  for (let i = 0; i < str.length; i++) {
+    const value = str[i]
+    if (['[', '{', '('].indexOf(value) > -1) {
+      stack.push(value)
+    } else {
+      const last = stack[stack.length - 1]
+      if (
+        (last === '[' && value === ']') ||
+        (last === '{' && value === '}') ||
+        (last === '(' && value === ')')
+      ) {
+        stack.pop()
+      } else {
+        return false
+      }
+    }
+  }
+  console.log('stack >>> ', stack.length)
+  return !stack.length
 }
-console.log(fn(arr, 3))
+
+console.log(fn(str))

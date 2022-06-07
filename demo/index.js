b4fce8135c9b9d516685a8e45caf2567fb3a2c4d
aicherish0320 <aicherish0320@163.com>
2022-06-07 17:16:52 +0800

    把一个数组旋转 k 步

--- demo/index.js
+++ demo/index.js
@@ -1,46 +1,11 @@
 /**
- * 查找字符串中重复最多的字符和次数
+ * 把一个数组旋转 K 步
  */
 
-const str = 'aabbccceeee44444ppppppp'
-
-const fn = (arr) => {
-  let i, j
-  const length = arr.length
-  const ret = {
-    char: '',
-    length: 0
-  }
-
-  let tempLength = 0
-
-  for (i = 0, j = 0; i < length; i++) {
-    if (arr[i] === arr[j]) {
-      tempLength++
-
-      if (i === length - 1) {
-        if (tempLength > ret.length) {
-          ret.length = tempLength
-          ret.char = str[j]
-        }
-      }
-
-      continue
-    }
-
-    // 不相等
-    if (tempLength > ret.length) {
-      ret.length = tempLength
-      ret.char = str[j]
-    }
-
-    tempLength = 0
-
-    j = i
-    i--
-  }
-
-  return ret
+const arr = [1, 2, 3, 4, 5, 6]
+const fn = (arr, k) => {
+  const a1 = arr.slice(0, k)
+  const a2 = arr.slice(k)
+  return a2.concat(a1)
 }
-
-console.log(fn(str))
+console.log(fn(arr, 3))
